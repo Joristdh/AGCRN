95dc7150768d97a98a09f7d2733fd02c4513c300
Joris <5628733-Joristdh@users.noreply.gitlab.com>
2021-10-27 20:14:34 +0200

    Save result

--- data/filter_sensors.py
+++ data/filter_sensors.py
@@ -8,7 +8,6 @@ sensors = np.c_[ids[:, 0], file]
 
 
 def get_most_redundant_sensor():
-    print('Sensors remaining:', len(sensors))
     amount_of_short_distances_per_sensor = {}
     for a in sensors:
 
@@ -18,17 +17,16 @@ def get_most_redundant_sensor():
                 short_distances_per_sensor.append(x)
         amount_of_short_distances_per_sensor[a[0]] = len(short_distances_per_sensor)
 
+    print('Adjacent sensors remaining:', len(amount_of_short_distances_per_sensor))
     return not amount_of_short_distances_per_sensor or \
            max(amount_of_short_distances_per_sensor.items(), key=lambda k: k[1])[0]
 
 
 sensor = True
 while sensor:
-    print(gc.get_count())
     sensors = np.delete(sensors, (np.searchsorted(sensors[:, 0], sensor)), 0)
     sensor = get_most_redundant_sensor()
     gc.collect()
-    print(gc.get_count())
 
+np.save('sensors_to_keep', sensors)
 print(len(sensors))
-print(sensors)
