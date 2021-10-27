3ad891919ad375d11c2024e4e5b24d4fe2440162
Joris <5628733-Joristdh@users.noreply.gitlab.com>
2021-10-27 18:18:42 +0200

    Fix join_matrix.py and remove faulty shortcut

--- data/RAW/join_matrix.py
+++ data/RAW/join_matrix.py
@@ -3,4 +3,4 @@ import numpy as np
 file1 = np.load('distance_matrix1.npy')
 file2 = np.load('distance_matrix2.npy')
 
-np.save('distance_matrix', np.concatenate(file1, file2))
+np.save('distance_matrix', np.concatenate((file1, file2)))
--- data/filter_sensors.py
+++ data/filter_sensors.py
@@ -8,12 +8,12 @@ file_with_id = np.c_[ids[:, 0], file]
 def get_most_redundant_sensor(sensors):
     print('Sensors remaining:', len(sensors))
     amount_of_short_distances_per_sensor = {}
-    for i, a in enumerate(sensors):
+    for a in sensors:
 
         short_distances_per_sensor = []
-        for x in range(i + 2, len(a)):
-            if a[x] <= 5.6327:  # 3.5 miles is 5.6327 km
-                short_distances_per_sensor.append(a[x])
+        for x in a:
+            if 5.6327 >= x > 0:  # 3.5 miles is 5.6327 km
+                short_distances_per_sensor.append(x)
         amount_of_short_distances_per_sensor[a[0]] = len(short_distances_per_sensor)
 
     return not amount_of_short_distances_per_sensor or \
