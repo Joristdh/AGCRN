68ff8cb844f9a98d7e3f392b42d7e1f125931cc8
Joris <5628733-Joristdh@users.noreply.gitlab.com>
2021-10-28 09:42:55 +0200

    Keep track of removed sensors + add try except + adjust column index

--- data/filter_sensors.py
+++ data/filter_sensors.py
@@ -1,4 +1,5 @@
 import gc
+from datetime import datetime
 
 import numpy as np
 
@@ -15,18 +16,28 @@ def get_most_redundant_sensor():
         for x in a:
             if 5.6327 >= x > 0:  # 3.5 miles is 5.6327 km
                 short_distances_per_sensor.append(x)
-        amount_of_short_distances_per_sensor[a[0]] = len(short_distances_per_sensor)
+        if len(short_distances_per_sensor) > 0:
+            amount_of_short_distances_per_sensor[a[0]] = len(short_distances_per_sensor)
 
-    print('Adjacent sensors remaining:', len(amount_of_short_distances_per_sensor))
+    print(datetime.now(), 'Adjacent sensors remaining:', len(amount_of_short_distances_per_sensor))
     return not amount_of_short_distances_per_sensor or \
            max(amount_of_short_distances_per_sensor.items(), key=lambda k: k[1])[0]
 
 
-sensor = True
+removed_sensors = []
+sensor = get_most_redundant_sensor()
 while sensor:
-    sensors = np.delete(sensors, (np.searchsorted(sensors[:, 0], sensor)), 0)
-    sensor = get_most_redundant_sensor()
-    gc.collect()
+    nr = np.searchsorted(sensors[:, 0], sensor)
+    print(datetime.now(), 'Deleting node:', int(sensor), 'at:', nr)
+    try:
+        sensors = np.delete(np.delete(sensors, nr, 0), nr + 1, 1)
+        removed_sensors.append(sensor)
+        sensor = get_most_redundant_sensor()
+        gc.collect()
+    except:
+        np.save('remaining_sensors', sensors)
+        np.save('removed_sensors', removed_sensors)
 
 np.save('sensors_to_keep', sensors)
+np.save('removed_sensors', removed_sensors)
 print(len(sensors))
